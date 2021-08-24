bd130390169d6b7bc3d6f6da0a410e4ebd774ca0
gabriel3c <gbrei@hotmail.com>
2021-08-24 12:09:43 -0300

    feat: creat calculator functionalities

--- script.js
+++ script.js
@@ -1,5 +1,27 @@
-let button = document.querySelectorAll('button')
+function insert(num) {
+  let number = document.getElementById('result').innerHTML
+  if (number.length < 11) {
+    // limita tamanho da string na calculadora
+    let number = document.getElementById('result').innerHTML
+    document.getElementById('result').innerHTML = number + num
+  }
+}
 
-button.forEach((element) => {
-  console.log(element)
-})
+function resolve() {
+  let expression = document.getElementById('result').innerHTML
+  result = eval(expression)
+  document.getElementById('result').innerHTML = result
+  return console.log(expression + ' = ' + result)
+}
+
+function cleanAll() {
+  document.getElementById('result').innerHTML = ''
+}
+
+function cleanLast() {
+  let number = document.getElementById('result').innerHTML
+  document.getElementById('result').innerHTML = number.substring(
+    0,
+    number.length - 1
+  )
+}
