1c92255d842511704a88b493e0683adf9ddb766c
gabriel3c <gbrei@hotmail.com>
2021-10-08 11:15:03 -0300

    feat: change title for english words, remove ideias.js file

--- ideias.js
+++
@@ -1,8 +0,0 @@
-for (i = 1; i < 12; i++) {
-  let positionResult = document.getElementById(i).innerHTML
-  if (!(positionResult === '')) {
-    document.getElementById('i+1').innerHTML = printResult
-  } else {
-    document.getElementById('i').innerHTML = printResult
-  }
-}
